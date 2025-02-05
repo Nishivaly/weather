a86887b8e5c9a109dc078749fdbb42db2fd3ddb5
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-02-05 03:38:17 -0300

    Refactor into away/async

--- src/api.js
+++ src/api.js
@@ -1,19 +1,19 @@
 import processData from "./process";
 import populateTable from "./display";
 
-export default (location, unit) => {
-  fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=RBZM57BNTJE7E5CQ9YQG8P3UJ&contentType=json`,
-    {
-      method: "GET",
-      headers: {},
-    },
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return processData(data);
-    })
-    .then(([place, cleanData]) => populateTable(cleanData, place, unit))
-    .catch((error) => console.log(error));
+export default async (location, unit) => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unit}&key=RBZM57BNTJE7E5CQ9YQG8P3UJ&contentType=json`,
+      {
+        method: "GET",
+        headers: {},
+      },
+    );
+    const data = await response.json();
+    const [place, cleanData] = processData(data);
+    populateTable(cleanData, place, unit);
+  } catch (error) {
+    console.log(error);
+  }
 };
